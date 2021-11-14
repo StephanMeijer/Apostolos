fc257830e529f96be24f1a5c0bbe58a3d503c19a
Stephan Meijer <me@stephanmeijer.com>
2021-11-14 21:16:49 +0100

    test ConfigLoader

---
+++ tests/Service/ConfigLoaderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\DataStructure;
+
+use App\Service\ConfigLoader;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ConfigLoaderTest extends KernelTestCase
+{
+    public function testDecodingConfig(): void
+    {
+        $loader = new ConfigLoader();
+
+        $this->assertEquals(
+            [
+                'calendars' => [
+                    'Client1' => 'https://calendar.google.com/calendar/ical/692cf04a-1d49-4772-b09b-0ec324853277/basic.ics',
+                    'Client2' => 'https://calendar.google.com/calendar/ical/0f26df45-f42e-4e56-b495-67cb11f5ea91/basic.ics'
+                ]
+            ],
+            $loader->load(__dir__ . "/Fixtures/config.yml")
+        );
+    }
+}
