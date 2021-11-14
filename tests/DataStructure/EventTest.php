38213bd7b275ff701a5b7dac0ddd1c95270604ff
Stephan Meijer <me@stephanmeijer.com>
2021-11-14 20:27:33 +0100

    First test on Event

---
+++ tests/DataStructure/EventTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\DataStructure;
+
+use DateTime;
+use App\DataStructure\Event;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class EventTest extends KernelTestCase
+{
+    public function testConstructor(): void
+    {
+        $start = $this->createMock(DateTime::class);
+        $end = $this->createMock(DateTime::class);
+
+        $description = 'test';
+
+        $event = new Event($start, $end, $description);
+
+        $this->assertEquals($start, $event->start);
+        $this->assertEquals($end, $event->end);
+        $this->assertEquals($description, $event->description);
+    }
+
+    public function testSetters(): void
+    {
+        $start = $this->createMock(DateTime::class);
+        $end = $this->createMock(DateTime::class);
+
+        $description = 'test';
+
+        $event = new Event($start, $end, $description);
+
+        $newStart = $this->createMock(DateTime::class);
+        $newEnd = $this->createMock(DateTime::class);
+
+        $newDescription = 'test';
+
+        $event->start = $newStart;
+        $event->end = $newEnd;
+        $event->description = $newDescription;
+
+        $this->assertEquals($newStart, $event->start);
+        $this->assertEquals($newEnd, $event->end);
+        $this->assertEquals($newDescription, $event->description);
+    }
+}
