af525275024a6015635a8f5098aac1f8774733f2
Stephan Meijer <me@stephanmeijer.com>
2021-12-28 23:11:12 +0200

    New DataStructure CalendarConfiguration

---
+++ src/DataStructure/CalendarConfiguration.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataStructure;
+
+class CalendarConfiguration {
+    public function __construct(
+        public readonly string $name,
+        public readonly string $url
+    ) {}
+}
